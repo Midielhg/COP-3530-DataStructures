8c60fb80d2638624c0c75b209886dbcdca9af33f
Midiel Henriquez <Midielhg@gmail.com>
2022-11-06 18:20:10 -0500

    Searchine Algorithm

---
+++ Assignment 3/Main.java
@@ -0,0 +1,59 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        double duration1 = 0;//duration of algorithm 1
+        double duration2 = 0;//duration of algorithm 2
+        int k = 0;//number of elements in dataSource
+        //create CVS file to print the results 
+        String outputFilename = "output.csv";//name of the file
+        PrintWriter output = null;//print writer
+        //open output stream
+        try
+        {
+            output = new PrintWriter(new FileWriter(outputFilename));
+        } catch (IOException ex)
+        {
+            System.exit(1);
+        }
+    
+        System.out.println("\nk" + "\t" + "Algorithm 1" + "\t" + "Algorithm 2 " );//print the header
+
+        for (int j = 0; j < 50 ;j++){//loop until algorithm 2 takes less time than algorithm 1
+            k++;//increment k by 1 
+
+            int[] dataset = new int[k];//create dataset array
+            SearchingAlgorithms.fillArray(dataset, 20000000);//fill the array with random numbers between 0 and sixe of dataset
+            int[] dataSource = new int[20000000];//create the data source
+            SearchingAlgorithms.fillArray(dataSource, 40000000);//fill the data source with random numbers between 0 and 2*size of dataset
+
+            //Algorithm 1
+            long startTime1 = System.nanoTime();//start time for algorithm 1
+                for (int i = 0; i < dataSource.length; i++) {//for loop to search the data source
+                    if (SearchingAlgorithms.sequentialSearch(dataset, dataSource[i])) {//
+                    }
+                }
+            long endTime1 = System.nanoTime();//end time for algorithm 1
+            duration1 = (endTime1 - startTime1)/1000000000.0;//duration for algorithm 1
+
+            //Algorithm 2
+            long startTime2 = System.nanoTime();//start time for algorithm 2
+                //call quicksort from searching algorithms class
+                SearchingAlgorithms.quickSort(dataset);//sort the array
+                    for (int i = 0; i < dataSource.length; i++) {//for loop to search the data source
+                        if (SearchingAlgorithms.binarySearch(dataset, dataSource[i])) {
+                        }
+                    }
+            long endTime2 = System.nanoTime();//end time for algorithm 2
+            duration2 = (endTime2 - startTime2) / 1000000000.0;//duration for algorithm 2
+
+            output.println(k + "," + duration1 + "," + duration2);//print the results to the .CVS file
+            System.out.println(k + "\t" + duration1 + "\t" + duration2);//print the results to the console            
+        }
+
+    //close output stream
+    output.close();
+    }
+}
---
+++ Assignment 3/SearchingAlgorithms.java
@@ -0,0 +1,88 @@
+import java.util.Random;
+
+public class SearchingAlgorithms {//
+    public static boolean sequentialSearch(int[] list, int x) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean binarySearch(int[] list, int x) {
+        int low = 0;
+        int high = list.length - 1;
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (x < list[mid]) {
+                high = mid - 1;
+            } else if (x == list[mid]) {
+                return true;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    //quick sort
+    public static void quickSort(int[] list){
+        quicksort(list, 0, list.length - 1);
+    }
+
+    private static void quicksort(int[] list, int begin, int end){
+        int temp;
+        int pivot = findPivotLocation(begin, end);
+        // swap list[pivot] and list[end]
+        temp = list[pivot];
+        list[pivot] = list[end];
+        list[end] = temp;
+        pivot = end;
+        int i = begin,
+        j = end - 1;
+        boolean iterationCompleted = false;
+        while (!iterationCompleted){
+            while (list[i] < list[pivot])
+                i++;
+            while ((j >= 0) && (list[pivot] < list[j]))
+                j--;
+            if (i < j){
+                //swap list[i] and list[j]
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                i++;
+                j--;
+            } else
+                iterationCompleted = true;
+        }
+
+        //swap list[i] and list[pivot]
+        temp = list[i];
+        list[i] = list[pivot];
+        list[pivot] = temp;
+
+        if (begin < i - 1) quicksort(list, begin, i - 1);
+        if (i + 1 < end) quicksort(list, i + 1, end);
+    }
+
+    //computes the pivot
+    private static int findPivotLocation(int b, int e){
+        return (b + e) / 2;
+    }
+
+    public static void fillArray(int[] list, int range) {
+        Random random = new Random();
+        for (int i = 0; i < list.length; i++) {
+            list[i] = random.nextInt(range);
+        }
+    }
+
+    public static void printArray(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
