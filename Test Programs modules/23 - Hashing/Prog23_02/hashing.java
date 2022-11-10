7ac20dfd58620091c4f6745eb3237eb8281ebcb3
Midiel Henriquez <Midielhg@gmail.com>
2022-11-09 20:09:15 -0500

    Commit

---
+++ Test Programs modules/23 - Hashing/Prog23_02/hashing.java
@@ -0,0 +1,49 @@
+//Implement a Has TAble ADT to store Strings. Resolve collitions using chaning 
+
+
+public class hashing {
+    public static void main(String[] args){
+        String[] keywords = {"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while"};///array of keywords
+        int[] hashValues = new int[keywords.length];//array to store hash values
+        int hashTableSize = 67;//size of the hash table
+        int collisions = 0;//counter for number of collisions
+        for (int i = 0; i < keywords.length; i++){//loop to compute hash values
+            hashValues[i] = hash(keywords[i], hashTableSize);//compute hash values
+            System.out.println(keywords[i] + " " + hashValues[i]);//print the hash values
+        }
+        for (int i = 0; i < hashValues.length; i++){//loop to check for collisions
+            for (int j = i + 1; j < hashValues.length; j++){//loop to check for collisions
+                if (hashValues[i] == hashValues[j]){//if there is a collision
+                    //print the value with collition 
+                    System.out.println("Collision between " + keywords[i] + " and " + keywords[j] + " with hash value " + hashValues[i]);
+                    collisions++;//increment the counter
+                }
+            }
+        }
+        System.out.println("Total Collisions: " + collisions);    
+    }
+
+
+    public static int hash (String key, int tableSize){//hash function
+        int hashVal = 0;//hash value
+
+        for (int i =0; i < key.length(); i++){//loop to compute hash value
+            hashVal = 37 * hashVal + key.charAt(i);//compute hash value
+        }
+
+        hashVal %= tableSize;//modulus to get hash value
+        if(hashVal < 0){//if hash value is negative
+            hashVal += tableSize;//add table size to make it positive
+        }
+        return hashVal;//return hash value
+    }
+
+
+    //linked list class
+    public class LinkedList{}
+
+    //int tableSize
+    ]]
+
+    
+}
---
+++ Test Programs modules/23 - Hashing/Prog23_02/node.java
@@ -0,0 +1,5 @@
+package Test Programs modules.23 - Hashing.Prog23_02;
+
+public class node {
+    
+}
