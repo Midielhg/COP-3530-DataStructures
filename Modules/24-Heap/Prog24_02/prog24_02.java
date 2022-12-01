0016ab1c389f70a89f5e3e660db819f7e76ce535
Midiel Henriquez <Midielhg@gmail.com>
2022-12-01 18:40:43 -0500

    Changes

---
+++ Modules/24-Heap/Prog24_02/prog24_02.java
@@ -0,0 +1,14 @@
+//implemetn a Heap-based ProprityQueue class. Make the element tupe a s(simple Person class. Test it.
+public class prog24_02 {
+    //main method
+    public static void main(String[] args){
+
+    }
+
+    //person class
+    public static class Person{}
+
+
+    //PriorityQueue Class
+
+}
---
+++ Modules/28-Graps/prog28_01.java
@@ -0,0 +1,6 @@
+/*Implement a Digraph class using the adjacency matrix to represent a digraph. Test your class. Consider implementing methods addVertex(v), removeVertex(v), addEdge(e), removeEdge(e), findAdjVertices(v), getValue(v), getWeight(e), and toString() */
+
+
+public class prog28_01 {
+        
+}
